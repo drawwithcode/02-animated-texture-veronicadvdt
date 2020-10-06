5d69cadf72207d374c245337e55ada7192c66f14
veronicadvdt <veronicadetoni@gmail.com>
2020-10-06 23:55:32 +0200

    Animated_Texture_V001

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
-function preload(){
-  // put preload code here
-}
+const colorPalette = ["red", "black", "grey", "orange"];
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-  // put setup code here
+
 }
 
 function draw() {
-  // put drawing code here
+
+  noStroke();
+  let x = random()*width;
+  let y = random()*height;
+  let r = random (10,60);
+  let col = random (colorPalette);
+
+  fill (color(col))
+
+  ellipse(x, y, r);
+
 }
