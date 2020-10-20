b17529b7cc50ed2a6b61e7040df5bc522f894b09
veronicadvdt <veronicadetoni@gmail.com>
2020-10-20 12:08:58 +0200

    Animated_Texture_V001

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
-const colorPalette = ["red", "black", "grey", "orange"];
+var colorList = ['DodgerBlue','SlateBlue','Sandybrown','Tomato', 'Hotpink', 'Khaki', 'Lightseagreen', 'Powderblue'];
+
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
 
+    createCanvas(windowWidth,windowHeight);
+    frameRate(5);
 }
 
 function draw() {
-
-  noStroke();
-  let x = random()*width;
-  let y = random()*height;
-  let r = random (10,60);
-  let col = random (colorPalette);
-
-  fill (color(col))
-
-  ellipse(x, y, r);
+for(var x = 0; x < 4000; x += 65) {
+    for(var y = 0; y < 1000; y += 65) {
+    var index = floor(random() * colorList.length);
+    var colorHex = colorList[index];
+    fill(color(colorHex));
+    stroke(color("black"));
+    strokeWeight(30);
+    ellipse(x,y,50,50);
+    }
+  }
 
 }
